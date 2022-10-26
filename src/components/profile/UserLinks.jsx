46220cf05698f86e6145906d4783c8e27bb9b53e
Akpokighe Marvellous O <m.ovigwe@yahoo.com>
2022-10-26 09:36:53 +0100

    React Basic Web Page

--- src/components/profile/UserLinks.jsx
+++ src/components/profile/UserLinks.jsx
@@ -10,7 +10,7 @@ const UserLinks = () => {
       {
         data && data.map((item) => (
           <ul key={item.id}>
-            <l className="text-[#101828] text-[18px] rounded-[8px] flex flex-col items-center justify-center w-[100%] bg-[#EAECF0] py-5 my-2"><a href={item.href} target="__blank" id={item.styleId}>{item.name}</a></l>
+            <l className="text-[#101828] text-[18px] rounded-[8px] flex flex-col items-center justify-center w-[100%] bg-[#EAECF0] py-5 my-5"><a href={item.href} target="__blank" id={item.styleId}>{item.name}</a></l>
           </ul>
         ))
       }
